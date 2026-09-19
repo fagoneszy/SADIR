ab5ea247bccf5fcfbda586d65938651bdb68b06e
Test User <test@test.com>
2026-09-19 10:43:48 -0300

    chore(repo): remove legacy artifacts and generated files

--- NADIR-starter/nadir/include/nadir/render/line.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <nadir/render/framebuffer.hpp>
-
-namespace nadir::render {
-
-void line(Framebuffer& fb, int x0, int y0, int x1, int y1);
-
-}
--- NADIR/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/faguinho/Documents/SADIR/NADIR
-# Build directory: C:/Users/faguinho/Documents/SADIR/NADIR/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[nadir_tests]=] "C:/Users/faguinho/Documents/SADIR/NADIR/build/nadir_tests.exe")
-set_tests_properties([=[nadir_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/faguinho/Documents/SADIR/NADIR/CMakeLists.txt;63;add_test;C:/Users/faguinho/Documents/SADIR/NADIR/CMakeLists.txt;0;")
